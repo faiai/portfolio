b1cbfe7fada3ceb9b9e597141a057c36c7466b0e
faiai <49914190+faiai@users.noreply.github.com>
2021-02-22 14:32:29 -0500

    uploaded unedited orange png

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import artwork from '../imgs/Untitled_Artwork.png'
 
 const Home = () => {
     return (
         <div style={{display:'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
-           <h1>Home</h1> 
+           {/* <img src={require('../imgs/Untitled_Artwork.png')}  alt="peachy"/> */}
+           <img src={artwork}  alt="peachy"/>
         </div>
     )
 }
